cddb88840ff87f5709c509993b1c3045afb434ae
AmedEspinosa <amed.espinosa@mykenzie.snhu.edu>
2022-06-24 16:53:40 -0400

    added the ArtworkResponse class

--- Application/src/main/java/com/kenzie/appserver/controller/model/ArtworkResponse.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/ArtworkResponse.java
@@ -1,2 +1,75 @@
-package com.kenzie.appserver.controller.model;public class ArtworkResponse {
+package com.kenzie.appserver.controller.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ArtworkResponse {
+
+    @JsonProperty("artistName")
+    private String artistName;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("dateCreated")
+    private String dateCreated;
+
+    @JsonProperty("isSold")
+    private boolean isSold;
+
+    @JsonProperty("isForSale")
+    private boolean isForSale;
+
+    @JsonProperty("price")
+    private int price;
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public boolean isSold() {
+        return isSold;
+    }
+
+    public void setSold(boolean sold) {
+        isSold = sold;
+    }
+
+    public boolean isForSale() {
+        return isForSale;
+    }
+
+    public void setForSale(boolean forSale) {
+        isForSale = forSale;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }

9e1085b12e34a1c6e63055c3c4892cd4e43d99a4
Jon Strand <jon.strand@mykenzie.snhu.edu>
2022-07-08 16:57:46 -0500

    front end changes for view/delete

--- Frontend/src/pages/viewPage.js
+++ Frontend/src/pages/viewPage.js
@@ -9,7 +9,7 @@ class ViewPage extends BaseClass {
 
     constructor() {
         super();
-        this.bindClassMethods(['onGet', 'onViewAllArtwork', 'renderExample'], this);
+        this.bindClassMethods(['onGet', 'onGetAll', 'renderExample'], this);
         this.dataStore = new DataStore();
     }
 
@@ -18,6 +18,7 @@ class ViewPage extends BaseClass {
      */
     async mount() {
         document.getElementById('view-artwork-form').addEventListener('submit', this.onGet);
+//        document.getElementById('view-all-artwork-form').addEventListener('submit', this.onGetAll);
 
         this.client = new ViewArtworkClient();
 
@@ -25,54 +26,56 @@ class ViewPage extends BaseClass {
     }
 
     // Render Methods --------------------------------------------------------------------------------------------------
-//      TODO : RE-CODE RENDERING
     async renderExample() {
-//        let resultArea = document.getElementById("result-info");
-//
-//        const example = this.dataStore.get("example");
-//
-//        if (example) {
-//            resultArea.innerHTML = `
-//                <div>ID: ${example.id}</div>
-//                <div>Name: ${example.name}</div>
-//            `
-//        } else {
-//            resultArea.innerHTML = "No Item";
-//        }
+        let resultArea = document.getElementById("view-artwork-form");
+
+        const artwork = this.dataStore.get("artwork");
+
+        if (artwork) {
+            resultArea.innerHTML = `
+                <div>ID: ${artwork.id}</div>
+                <div>Artist Name: ${artwork.artistName}</div>
+                <div>Title: ${artwork.title}</div>
+                <div>Date Created: ${artwork.dateCreated}</div>
+                <div>Date Posted: ${artwork.datePosted}</div>
+                <div>Height: ${artwork.height}</div>
+                <div>Width: ${artwork.width}</div>
+                <div>Is Sold: ${artwork.isSold}</div>
+                <div>Is For Sale: ${artwork.isForSale}</div>
+                <div>Price: ${artwork.price}</div>
+            `
+        } else {
+            resultArea.innerHTML = "No Item";
+        }
     }
 
     // Event Handlers --------------------------------------------------------------------------------------------------
-// TODO : RE-WRITE TO VIEW ALL ARTWORK
-    async onViewAllArtwork(event) {
+// TODO : VIEW ALL ARTWORK
+    async onGetAll(event) {
+        const artworks = await this.client.getAllArtwork(this.errorHandler)
+
+        if (artworks && artworks.length > 0) {
+            for (const artwork of artworks) {
+                await this.getArtwork(artwork.id);
+            }
+        }
+        this.dataStore.set("artworks", artworks);
+    }
+
+    async onGet(event) {
         // Prevent the page from refreshing on form submit
         event.preventDefault();
 
-        let id = document.getElementById("id-field").value;
-        this.dataStore.set("example", null);
+        let id = document.getElementById("view-artwork-id").value;
+        this.dataStore.set("artwork", null);
 
-        let result = await this.client.getExample(id, this.errorHandler);
-        this.dataStore.set("example", result);
+        let result = await this.client.getArtwork(id, this.errorHandler);
+        this.dataStore.set("artwork", result);
         if (result) {
             this.showMessage(`Got ${result.name}!`)
         } else {
             this.errorHandler("Error doing GET!  Try again...");
         }
-    }
-
-    async onGet(event) {
-//        // Prevent the page from refreshing on form submit
-//        event.preventDefault();
-//
-//        let id = document.getElementById("id-field").value;
-//        this.dataStore.set("example", null);
-//
-//        let result = await this.client.getExample(id, this.errorHandler);
-//        this.dataStore.set("example", result);
-//        if (result) {
-//            this.showMessage(`Got ${result.name}!`)
-//        } else {
-//            this.errorHandler("Error doing GET!  Try again...");
-//        }
     }
 }
 
